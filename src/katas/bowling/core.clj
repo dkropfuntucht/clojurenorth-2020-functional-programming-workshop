8b19d4f3b31b0a813dd635402afa9eaf10745df7
Damon Kropf-Untucht <dkropfuntucht@teksavvy.com>
2020-06-25 12:14:57 -0400

    Add working Monty Hall and 50% of bowling

--- src/katas/bowling/core.clj
+++ src/katas/bowling/core.clj
@@ -1,4 +1,86 @@
 (ns katas.bowling.core)
 
+(defn characterize-frames
+  [rolls]
+  (let [first-pass (reduce
+                    (fn [{:keys [current frame frames in-frame-count]} roll]
+                      (cond (= roll 10)
+                            {:current        0
+                             :frame          (inc frame)
+                             :frames         (conj frames {:rolls       1
+                                                           :type        :strike
+                                                           :frame-num   (inc frame)
+                                                           :local-score 10})
+                             :in-frame-count 0}
+
+                            (= (+ roll current) 10)
+                            {:current       0
+                             :frame         (inc frame)
+                             :frames        (conj frames {:rolls       2
+                                                          :type        :spare
+                                                          :frame-num   (inc frame)
+                                                          :local-score 10})
+                             :in-frame-count 0}
+
+                            (= in-frame-count 0)
+                            {:current        roll
+                             :frame          frame
+                             :frames         frames
+                             :in-frame-count 1}
+
+                            :else
+                            {:current        0
+                             :frame          (inc frame)
+                             :frames         (conj frames {:rolls       2
+                                                           :type        :whatever-you-call-this
+                                                           :frame-num   (inc frame)
+                                                           :local-score (+ roll current)})
+                             :in-frame-count 0}))
+                    {:current        0
+                     :in-frame-count 0
+                     :frame          0
+                     :frames         []}
+                    rolls)]
+    (if (zero? (:current first-pass))
+      (:frames first-pass)
+      (conj (:frames first-pass)
+            {:rolls       1
+             :type        :bonus-roll
+             :frame-num   (inc (:frame first-pass))
+             :local-score (:current first-pass)}))))
+
+(defn look-forward
+  [rolls frame-num step]
+  (let [target-idx (+ (dec frame-num) step)]
+    (if (> target-idx (dec (count rolls)))
+      {:type :not-there
+       :local-score 0}
+      (nth rolls target-idx))))
+
+(defn score
+  [rolls]
+  (reduce
+   (fn [{:keys [total-score]} {:keys [type frame-num local-score] :as roll}]
+     {:total-score (condp = type
+
+                     :spare
+                     (+ total-score
+                        local-score
+                        (:local-score (look-forward rolls frame-num 1)))
+
+                     :strike
+                     (+ total-score
+                        local-score
+                        (:local-score (look-forward rolls frame-num 1))
+                        (:local-score (look-forward rolls frame-num 2)))
+
+                     (+ total-score local-score))})
+   {:total-score 0}
+   rolls))
+
 (defn total-score
-  [rolls])
+  [rolls]
+  (-> rolls
+      characterize-frames
+      score
+      :total-score))
--- src/katas/monty_hall/core.clj
+++ src/katas/monty_hall/core.clj
@@ -1,3 +1,44 @@
 (ns katas.monty-hall.core)
 
-(defn simulate [])
+(defn make-doors []
+  (shuffle [::goat ::goat ::car]))
+
+(defn remove-door
+  [doors idx]
+  (vec (concat (subvec doors 0 idx)
+               (subvec doors (inc idx)))))
+
+(defn pick-one
+  [doors]
+  (let [pick-idx (rand-int (count doors))]
+    {::pick  (get doors pick-idx)
+     ::doors (remove-door doors pick-idx)}))
+
+(defn pick-subsequent
+  [result]
+  (if (= (::pick result) ::car)
+    result
+    (pick-one (::doors result))))
+
+(defn count-cars
+  [result]
+  (->> result
+       (map ::pick)
+       (filter #(= % ::car))
+       count))
+
+(defn simulate []
+  (let [attempts       1000
+        stay-results   (count-cars
+                        (for [_ (range 0 attempts)]
+                          (-> (make-doors)
+                              pick-one)))
+        switch-results (count-cars
+                        (for [_ (range 0 attempts)]
+                          (-> (make-doors)
+                              pick-one
+                              pick-subsequent)))]
+    {:stay   {false switch-results
+              true  stay-results}
+     :switch {true  switch-results
+              false stay-results} }))
